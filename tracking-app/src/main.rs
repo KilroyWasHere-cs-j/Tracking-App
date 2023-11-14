0fdcc9cc2e3121a8c1d7154a6c359c9569723e5e
Gabriel Tower <gabriel.tower@baxter-academy.org>
2023-11-14 12:58:43 -0500

    Old Rocket

--- tracking-app/src/main.rs
+++ tracking-app/src/main.rs
@@ -1,15 +1,13 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
 #[macro_use] extern crate rocket;
 
-// this is our get route which will be requested at the "/" location wherever it is mounted
+
 #[get("/")]
-fn say_hello() -> &'static str {
-    "Hello, welcome to the api!"
+fn hello() -> &'static str {
+    "Hello, world!"
 }
 
-//noinspection ALL
-// start the web server and mount our get route at "/api". Can replace /api with anything
-// or just leave it as "/" as the default location
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![say_hello])
+fn main() {
+    rocket::ignite().mount("/", routes![hello]).launch();
 }
