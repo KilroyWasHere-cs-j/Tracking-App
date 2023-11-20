e6b3348029cf48246cbb1c43ee11fa2c63ecaa58
Gabriel Tower <gabriel.tower@baxter-academy.org>
2023-11-20 16:22:58 -0500

    Cleaned up the code

--- tracking-app/src/routes/index.rs
+++ tracking-app/src/routes/index.rs
@@ -1,8 +1,19 @@
 use rocket::State;
+use serde::{Deserialize, Serialize};
 use crate::DB;
+
+#[derive(Debug, Deserialize, Serialize)]
+struct DbInfo {
+    db_id: u32,
+    db_name: String,
+}
 
 #[get("/")]
 pub fn index(state: State<DB>) -> String {
-    let mut db = state.db.lock().unwrap();
-    format!("Current running database ID: {} Name: {}", db.get_id(), db.get_name())
+    let db = state.db.lock().unwrap();
+    let message = DbInfo {
+        db_id: db.get_id() as u32,
+        db_name: db.get_name().to_string(),
+    };
+    format!("{}", serde_json::to_string(&message).unwrap())
 }
